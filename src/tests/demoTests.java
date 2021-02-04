d0149e0beaa46ec1455366b6ba379fef9a730851
iObrovac <igor.obrovac990@gmail.com>
2021-02-04 21:37:59 +0100

    dodat navigate to

--- src/tests/demoTests.java
+++ src/tests/demoTests.java
@@ -3,12 +3,18 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-@Test
+
+
+
 public class demoTests {
+	
+	@Test
 public void demoTest() {
 	System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
 	
 	WebDriver driver = new ChromeDriver();
+	driver.navigate().to("https://www.udemy.com/");
+	
 	
 	
 }
